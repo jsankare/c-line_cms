f477cde9c83d579e89ebcf2356b27d17e802c67e
Brubru <jordan.sankare@gmail.com>
2024-09-25 18:56:59 +0200

    wip footer

--- www/Views/Templates/front-footer.php
+++ www/Views/Templates/front-footer.php
@@ -1,3 +1,27 @@
 <footer class="footer">
-    <p class="footer--hello">hello world footer</p>
+    <header>
+        <section class="footer--left">
+            <div class="footer--logo">
+                <a class="footer--logo__link" href="/"><img class="footer--logo__picture" src="/assets/logo.svg"></a>
+            </div>
+            <div>
+                <h4>Contact</h4>
+                <a href="#mailto">monmail@mail.com</a>
+            </div>
+            <div>
+                <ul>
+                    <li>facebook</li>
+                    <li>instagram</li>
+                    <li>twitter</li>
+                </ul>
+            </div>
+        </section>
+        <section class="footer--right">
+
+        </section>
+    </header>
+    <footer>
+
+    </footer>
+
 </footer>
