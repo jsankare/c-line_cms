c106d4194faa110ffca9949961e94c960d8de355
Jordan_Sankare <jordan.sankare@gmail.com>
2024-10-09 17:32:24 +0200

    minor updates

--- www/Views/Templates/front-footer.php
+++ www/Views/Templates/front-footer.php
@@ -37,6 +37,18 @@ $footerStyles = 'style="background-color: ' . ($backgroundColor ?? '') . '; font
                 <?php if (!empty($pages)): ?>
                     <h3 class="footer--right__title">Liste des pages :</h3>
                     <ul class="footer--right__list">
+                        <li class="footer--right__listItem">
+                            <a class="footer--right__listItemLink" href="/">Accueil</a>
+                        </li>
+                        <li class="footer--right__listItem">
+                            <a class="footer--right__listItemLink" href="/articles">Articles</a>
+                        </li>
+                        <li class="footer--right__listItem">
+                            <a class="footer--right__listItemLink" href="/products/show">Produits</a>
+                        </li>
+                        <li class="footer--right__listItem">
+                            <a class="footer--right__listItemLink" href="/gallery">Galerie</a>
+                        </li>
                         <?php foreach ($pages as $page): ?>
                             <li class="footer--right__listItem">
                                 <a class="footer--right__listItemLink" href="/page/<?= $page->getSlug(); ?>"><?= $page->getTitle(); ?></a>
@@ -49,6 +61,6 @@ $footerStyles = 'style="background-color: ' . ($backgroundColor ?? '') . '; font
     </header>
 
     <footer class="footer--footer">
-        <!-- Footer bottom content here -->
+        <span class="footer--footer__span">Creation <a class="footer--footer__link" href="https://www.jsankare.dev/">Jordan Sankare</a> 2024</span>
     </footer>
 </footer>
